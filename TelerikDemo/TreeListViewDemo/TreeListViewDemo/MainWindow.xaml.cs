b2c5ff1c3cb0c6e5ab5b17e34fc8c03bc8d08f49
wangchunlei <wcl_orange@163.com>
2014-05-27 16:23:11 +0800

    commit

---
+++ TelerikDemo/TreeListViewDemo/TreeListViewDemo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.GridView;
+
+namespace TreeListViewDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.radTreeListView.ItemsSource = WarehouseService.GetWarehouseData();
+        }
+
+        private void radTreeListView_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource.GetType() == typeof(GridViewCell))
+            {
+                // Starts the Edit on the row;
+                var grd = (RadTreeListView)sender;
+                grd.BeginEdit(e);
+            }
+        }
+
+        private void RadMenuItem_Click(object sender, Telerik.Windows.RadRoutedEventArgs e)
+        {
+            var test = new Test();
+            this.Content = test;
+        }
+    }
+}
